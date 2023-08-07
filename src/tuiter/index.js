dec13d21b1535710fbb731dba0835e52e8bf106b
Rahul Suresh <rahs98@gmail.com>
2023-08-06 22:38:09 -0400

    Session aware front end

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,16 +4,22 @@ import NavigationSidebar from "./navigation-sidebar";
 import HomeScreen from "./home-screen";
 import ExploreScreen from "./explore-screen";
 import BookmarksScreen from "./bookmarks-screen";
-import ProfileScreen from "./profile-screen";
+// import ProfileScreen from "./profile-screen";
 import WhoToFollowList from "./who-to-follow-list";
 import whoReducer from "./reducers/who-reducer";
 import tuitsReducer from "./reducers/tuits-reducer";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import ProfileScreen from "./user/profile-screen";
+import LoginScreen from "./user/login-screen";
+import RegisterScreen from "./user/register-screen";
+import authReducer from "./reducers/auth-reducer";
+
 const store = configureStore({
     reducer: {
         who: whoReducer,
         tuits: tuitsReducer,
+        user: authReducer,
     },
 });
 
@@ -36,6 +42,15 @@ function Tuiter() {
                             <Route
                                 path="/bookmarks"
                                 element={<BookmarksScreen />}
+                            />
+                            {/* <Route
+                                path="/profile"
+                                element={<ProfileScreen />}
+                            /> */}
+                            <Route path="/login" element={<LoginScreen />} />
+                            <Route
+                                path="/register"
+                                element={<RegisterScreen />}
                             />
                             <Route
                                 path="/profile"
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const NavigationSidebar = () => {
+    const { currentUser } = useSelector((state) => state.user);
     const { pathname } = useLocation();
     const [ignore, tuiter, active] = pathname.split("/");
     const links = [
@@ -10,7 +13,7 @@ const NavigationSidebar = () => {
         "messages",
         "bookmarks",
         "lists",
-        "profile",
+        // "profile",
         "more",
     ];
     return (
@@ -25,6 +28,38 @@ const NavigationSidebar = () => {
                     {link}
                 </Link>
             ))}
+            {!currentUser && (
+                <Link
+                    className={`list-group-item text-capitalize ${
+                        active === "login" ? "active" : ""
+                    }`}
+                    to="/tuiter/login"
+                >
+                    {" "}
+                    Login{" "}
+                </Link>
+            )}
+            {!currentUser && (
+                <Link
+                    className={`list-group-item text-capitalize ${
+                        active === "register" ? "active" : ""
+                    }`}
+                    to="/tuiter/register"
+                >
+                    Register
+                </Link>
+            )}
+            {currentUser && (
+                <Link
+                    className={`list-group-item text-capitalize ${
+                        active === "login" ? "profile" : ""
+                    }`}
+                    to="/tuiter/profile"
+                >
+                    {" "}
+                    Profile{" "}
+                </Link>
+            )}
         </div>
     );
 };
